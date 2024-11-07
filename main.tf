37ceb0f1f9b4d4c8d2af4d3e63bd2e53a4fc635b
Dubie <pheerlessliter@gmail.com>
2024-11-06 16:04:41 -0800

    Changing how the providers are called

--- main.tf
+++ main.tf
@@ -1,2 +1,12 @@
-provider "spacelift" {}
-provider "aws" {}
+terraform {
+  required_providers {
+    spacelift = {
+      source  = "spacelift-io/spacelift"
+      version = "1.17.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.74.0"
+    }
+  }
+}
