bbb68281d7289852da68534c670ac7ffb1baad55
Dubie <pheerlessliter@gmail.com>
2024-11-06 16:52:27 -0800

    Adding PR push messaging

---
+++ policies.tf
@@ -0,0 +1,10 @@
+resource "spacelift_policy" "pr_messaging" {
+  name = "PR Push commenting"
+  body = file("policies/pr_messaging.rego")
+  type = "PUSH"
+}
+
+resource "spacelift_policy_attachment" "pr_messaging" {
+  policy_id = spacelift_policy.pr_messaging.id
+  stack_id  = spacelift_stack.spacelift_testing.id
+}
