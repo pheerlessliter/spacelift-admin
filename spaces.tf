182ca8b03a61427cb4de1e3f7a2668be26e41f35
Dubie <pheerlessliter@gmail.com>
2024-11-07 10:09:24 -0800

    inherit entities

--- spaces.tf
+++ spaces.tf
@@ -1,5 +1,6 @@
 resource "spacelift_space" "poc" {
-  name            = "PoC"
-  parent_space_id = "root"
-  description     = "Proof of concept space for testing and discovery"
+  name             = "PoC"
+  parent_space_id  = "root"
+  description      = "Proof of concept space for testing and discovery"
+  inherit_entities = true
 }
